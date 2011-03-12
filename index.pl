dc5dff311a88be3ff47c1f969128770e7d98e449
JoshAshby <joshuaashby@joshashby.com>
2011-03-12 16:34:56 -0700

    left for a few and forgot if I commited or not... Not a good day for the brain.

--- index.pl
+++ index.pl
@@ -29,35 +29,35 @@ our $update_product;
 
 my $json = new JSON;
 
-do 'database.pl';
+#do 'database.pl';
 
 print $form->header();
 
 if ($type_of_query eq 'single_product_info') {
-$get_product->execute("$query","$query");
-$get_product->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
-while($get_product->fetch()){
-%text = ('id' => $id, 'name' => $name, 'description' => $description, 'barcode' => $barcode, 'quantity' => $quantity);
-print $json->encode(\%text);
-}
+   $get_product->execute("$query","$query");
+   $get_product->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
+   while($get_product->fetch()){
+      %text = ('id' => $id, 'name' => $name, 'description' => $description, 'barcode' => $barcode, 'quantity' => $quantity);
+      print $json->encode(\%text);
+   }
 } elsif ($type_of_query eq 'total_inventory') {
-$get_all_products->execute();
-$get_all_products->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
-while($get_all_products->fetch()){
-%text = ('id' => $id, 'name' => $name, 'description' => $description, 'barcode' => $barcode, 'quantity' => $quantity);
-print $json->encode(\%text);
-}
+   $get_all_products->execute();
+   $get_all_products->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
+   while($get_all_products->fetch()){
+      %text = ('id' => $id, 'name' => $name, 'description' => $description, 'barcode' => $barcode, 'quantity' => $quantity);
+      print $json->encode(\%text);
+   }
 } elsif ($type_of_query eq 'update_product') {
-$get_product->execute("$query","$query");
-$get_product->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
-while($get_product->fetch()){
-$new_quantity = $quantity + 1;
-}
-$update_product->execute("$new_quantity","$query","$query");
-$get_product->execute("$query","$query");
-$get_product->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
-while($get_product->fetch()){
-%text = ('id' => $id, 'name' => $name, 'description' => $description, 'barcode' => $barcode, 'quantity' => $quantity);
-print $json->encode(\%text);
-}
+   $get_product->execute("$query","$query");
+   $get_product->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
+   while($get_product->fetch()){
+      $new_quantity = $quantity + 1;
+   }
+   $update_product->execute("$new_quantity","$query","$query");
+   $get_product->execute("$query","$query");
+   $get_product->bind_columns(undef, \$id, \$name, \$description, \$barcode, \$quantity);
+   while($get_product->fetch()){
+      %text = ('id' => $id, 'name' => $name, 'description' => $description, 'barcode' => $barcode, 'quantity' => $quantity);
+      print $json->encode(\%text);
+   }
 }
