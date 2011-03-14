2fad4b900519f49e9d9fdfd1895013131cd62363
JoshAshby <joshuaashby@joshashby.com>
2011-03-13 19:01:20 -0600

    So far most of this has been a big sandbox for developing the base for the true client and API, however it's getting there. I'm at the point where I can start writing a basic GUI for it all and possibly a web front (still trying to decide if thats what I want to do). In terms of changed, the request.py script now has a lot more features, it's getting info on a product, scanning in and getting info, and adding a product, and scanning a new product in and adding it. The perl API hasn't changed much, only difference is for the time being test.pl is the main script instead of index.pl. client.py for the phone is still the same and will be for a few more days until i get to writing a GUI for the phone.

--- request.py
+++ request.py
@@ -7,9 +7,20 @@ import httplib, urllib
 import sys
 import bluetooth
 import virtkey
+import json
+
+last_query = 0
+
+y = 0
 
 v = virtkey.virtkey()
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+API_host = raw_input("Please enter API host address: ")
+if (API_host == ''):
+	API_host = 'localhost'
+	
+conn = httplib.HTTPConnection(API_host)
 
 def find_host():
 	for host, name in bluetooth.discover_devices(lookup_names=True):
@@ -29,27 +40,57 @@ def connect(host, port):
 def receive():
 	return sock.recv(100)
 	
-
+def send(product_name):
+	sock.send(product_name)
+	
 def scan():
 	while True:
 		query = receive()
 		params = urllib.urlencode({'type_of_query': 'single_product_info', 'query': query})
-		request(params)
-		if (sys.argv[2] == 'keyboard'):
+		data = request(params,query)
+		if (y):
 			for x in query:
 				v.press_unicode(ord(x))
 				v.release_unicode(ord(x))
 
-def request(params):
+def add(params):
 	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-	conn = httplib.HTTPConnection("localhost")
 	conn.request("POST", "/barcode-perl-API/test.pl", params, headers)
 	response = conn.getresponse()
 	data = response.read()
-	return data
 	conn.close()
+	new_data = json.loads(data)
+	print new_data
 
-type_of_query = sys.argv[1]
+def request(params,query):
+	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+	conn.request("POST", "/barcode-perl-API/test.pl", params, headers)
+	response = conn.getresponse()
+	data = response.read()
+	conn.close()
+	print data
+	if (data == ''):
+		new(query)
+	else:
+		new_data = json.loads(data)
+		print new_data
+	
+def new(query):
+	print "The product you scanned is not in the database, would you like to add it?"
+	var = raw_input("Y/N: ")
+	if (var == 'Y'):
+		name = raw_input("Enter Name: ")
+		description = raw_input("Enter Description: ")
+		quantity = raw_input("Enter Quantity: ")
+		params = urllib.urlencode({'type_of_query': 'add_new_product', 'name': name, 'description': description, 'query': query, 'quantity': quantity})
+		add(params)
+			
+def decrypt(json_data):
+	decrypt_json = json.loads(json_data)
+	return decrypt_json
+
+print "Types of querys: server\n total_inventory\n single_product_info\n update_product_quantity\n add_new_product"
+type_of_query = raw_input("Please enter a query type or quit:")
 
 if (type_of_query == 'server'):
 	host = find_host()
@@ -58,21 +99,31 @@ if (type_of_query == 'server'):
 	port = find_port(host)
 	connect(host,port)
 	scan()
-else:
-	params = urllib.urlencode({'type_of_query': type_of_query})
-	request(params)
 
 if (type_of_query == 'total_inventory') :
 	params = urllib.urlencode({'type_of_query': type_of_query})
 	request(params)
 
 if (type_of_query == 'single_product_info') :
-	query = sys.argv[2]
+	query = raw_input("Name: ")
 	params = urllib.urlencode({'type_of_query': type_of_query, 'query': query})
 	request(params)
 
 if (type_of_query =='update_product_quantity') :
-	query = sys.argv[2]
-	quantity = sys.argv[3]
+	query = raw_input("Name: ")
+	quantity = raw_input("Quantity: ")
 	params = urllib.urlencode({'type_of_query': type_of_query, 'query': query, 'quantity': quantity})
 	request(params)
+	
+if (type_of_query =='add_new_product') :
+	name = raw_input("Name: ")
+	description = raw_input("Description: ")
+	quantity = raw_input("Quantity: ")
+	host = find_host()
+	if (host == 'None'):
+		host = find_host()
+	port = find_port(host)
+	connect(host,port)
+	query = receive()
+	params = urllib.urlencode({'type_of_query': type_of_query, 'name': name, 'description': description, 'query': query, 'quantity': quantity})
+	add(params)
